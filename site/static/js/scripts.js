f404607854f9f6b0324746c23f6e6ed9c5bd49b4
alex-gurung <aag1234@gmail.com>
2018-09-08 09:26:18 -0400

    show

--- site/static/js/scripts.js
+++ site/static/js/scripts.js
@@ -105,16 +105,47 @@
 	  	//console.log(data.image);
 	  	photo.setAttribute('src', data.image);
 		}
-		var ctx = document.getElementById("emotionChart").getContext("2d");
 		if (data.labels){
+			var ctx = document.getElementById("emotionChart").getContext("2d");
 			console.log(data.labels);
 			var myChart = new Chart(ctx, {
 				type: 'bar',
 				data: {
-					labels: data.labels,
-					data: data.vals,
+						labels: data.labels,
+						datasets: [{
+								label: 'Emotion displayed',
+								data: data.vals,
+								backgroundColor: [
+										'rgba(255, 99, 132, 0.2)',
+										'rgba(54, 162, 235, 0.2)',
+										'rgba(255, 206, 86, 0.2)',
+										'rgba(75, 192, 192, 0.2)',
+										'rgba(153, 102, 255, 0.2)',
+										'rgba(255, 159, 64, 0.2)',
+										'rgba(255,99,132,1)',
+								],
+								borderColor: [
+										'rgba(255,99,132,1)',
+										'rgba(54, 162, 235, 1)',
+										'rgba(255, 206, 86, 1)',
+										'rgba(75, 192, 192, 1)',
+										'rgba(153, 102, 255, 1)',
+										'rgba(255, 159, 64, 1)',
+										'rgba(75, 192, 192, 0.2)',
+								],
+								borderWidth: 1
+						}]
 				},
-			});
+				options: {
+						scales: {
+								yAxes: [{
+										ticks: {
+												beginAtZero:true
+										}
+								}]
+						}
+				}
+		});
 		}
 	  takepicture();
 	}
