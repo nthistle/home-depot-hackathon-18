d505f733cfa2c8dc41c704490942495ab764284c
alex-gurung <aag1234@gmail.com>
2018-09-08 10:58:20 -0400

    added tests

--- site/tests_app.py
+++ site/tests_app.py
@@ -2,38 +2,52 @@ import unittest
 
 class TestApp(unittest.TestCase): 
 
-  # # initialization logic for the test suite declared in the test module
-  # # code that is executed before all tests in one test run
-  # @classmethod
-  # def setUpClass(cls):
-  #      pass 
+  # initialization logic for the test suite declared in the test module
+  # code that is executed before all tests in one test run
+  @classmethod
+  def setUpClass(cls):
+       pass 
 
-  # # clean up logic for the test suite declared in the test module
-  # # code that is executed after all tests in one test run
-  # @classmethod
-  # def tearDownClass(cls):
-  #      pass 
+  # clean up logic for the test suite declared in the test module
+  # code that is executed after all tests in one test run
+  @classmethod
+  def tearDownClass(cls):
+       pass 
 
-  # # initialization logic
-  # # code that is executed before each test
-  # def setUp(self):
-  #   pass 
+  # initialization logic
+  # code that is executed before each test
+  def setUp(self):
+    pass 
 
-  # # clean up logic
-  # # code that is executed after each test
-  # def tearDown(self):
-  #   pass 
+  # clean up logic
+  # code that is executed after each test
+  def tearDown(self):
+    pass 
 
-  # test method
-  def test_encode_decode_base64(self):
+  # # test method
+  # def test_encode_decode_base64(self):
 
-    from face_detection import image_to_b64, request_to_image
+  #   from face_detection import image_to_b64, request_to_image
 
-    testString = "data:image/png;base64,QUJDREVGR0hJSktMTU5PUFFSU1RVVldYWVoxMjM0NTY3ODkwMDk4NzY1NDMyMSFAIyQl"
+  #   testString = "data:image/png;base64,QUJDREVGR0hJSktMTU5PUFFSU1RVVldYWVoxMjM0NTY3ODkwMDk4NzY1NDMyMSFAIyQl"
 
-    outString = 
+  #   outString = ""
 
-    self.assertEqual(2, 2) 
+  #   self.assertEqual(2, 2) 
+  
+  def test_normalize_distance(self):
+    from normalize import find_distance
+    self.assertEqual(find_distance((-2, 2), (4, 2)), 6)
+  
+  def test_normalize_mid(self):
+    from normalize import find_mid
+    self.assertEqual(find_mid((-2, 2), (4, 2)), (1,2))
+  
+  def test_normalize_scale(self): #scales two points about their midpoint
+    from normalize import scale
+    self.assertEqual(scale(2.4, (64, 34), (36, 34)), ((83, 34), (16, 34)))
+  
+
 
 # runs the unit tests in the module
 if __name__ == '__main__':
