71a25ea838d7c7f6bd33518c828cb055dbd4d3ba
Mike Krause <mekva@outlook.com>
2018-09-08 10:58:37 -0400

    tests

--- site/tests_app.py
+++ site/tests_app.py
@@ -2,38 +2,46 @@ import unittest
 
 class TestApp(unittest.TestCase): 
 
-  # # initialization logic for the test suite declared in the test module
-  # # code that is executed before all tests in one test run
-  # @classmethod
-  # def setUpClass(cls):
-  #      pass 
+  # initialization logic for the test suite declared in the test module
+  # code that is executed before all tests in one test run
+  @classmethod
+  def setUpClass(cls):
+       pass 
 
-  # # clean up logic for the test suite declared in the test module
-  # # code that is executed after all tests in one test run
-  # @classmethod
-  # def tearDownClass(cls):
-  #      pass 
+  # clean up logic for the test suite declared in the test module
+  # code that is executed after all tests in one test run
+  @classmethod
+  def tearDownClass(cls):
+       pass 
 
-  # # initialization logic
-  # # code that is executed before each test
-  # def setUp(self):
-  #   pass 
+  # initialization logic
+  # code that is executed before each test
+  def setUp(self):
+    pass 
 
-  # # clean up logic
-  # # code that is executed after each test
-  # def tearDown(self):
-  #   pass 
+  # clean up logic
+  # code that is executed after each test
+  def tearDown(self):
+    pass 
 
-  # test method
-  def test_encode_decode_base64(self):
+  # TEST DECODING BASE64 STRINGS
+  def test_decode_base64(self):
+    from face_detection import decode_b64
 
-    from face_detection import image_to_b64, request_to_image
+    sample_request = "data:image/png;base64,QUJDREU="
+    outString = "ABCDE"
+    self.assertEqual(decode_b64(sample_request), outString) 
 
-    testString = "data:image/png;base64,QUJDREVGR0hJSktMTU5PUFFSU1RVVldYWVoxMjM0NTY3ODkwMDk4NzY1NDMyMSFAIyQl"
 
-    outString = 
+  # TEST ENCODING PICTURE AS B64 STRING
+  def test_encode_base64(self):
+    from face_detection import decode_b64
 
-    self.assertEqual(2, 2) 
+    in_vec = [[255, 255, 135, 1]]
+
+    sample_request = "data:image/png;base64,QUJDREU="
+    outString = "ABCDE"
+    self.assertEqual(decode_b64(sample_request), outString) 
 
 # runs the unit tests in the module
 if __name__ == '__main__':
