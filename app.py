ddb3d4378133eef279027357a82a14469dc57646
Mike Krause <mekva@outlook.com>
2018-09-07 20:46:13 -0400

    yes, even more directory setup

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 app = Flask(__name__)
 
@@ -7,3 +7,5 @@ CORS(app)
 @app.route("/")
 def serve_main_page():
 	return "wow look at this page"
+
+
